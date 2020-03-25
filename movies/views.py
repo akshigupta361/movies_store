07509f81a924613106265a4ed98d617420711164
akshigupta361 <guptaakshi361@gmail.com>
2020-03-25 18:30:59 +0530

    Add names of Movies and Genre

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello ")
+    movies= Movie.objects.all()
+    output= ','.join([m.title for m in movies])
+    return HttpResponse(output)
